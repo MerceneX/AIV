86aa0b237a67623255b069d26ed790d1be68e0e4
MarkoTheStudent <marko.gluhak@student.um.si>
2019-03-17 17:58:47 +0100

    Added interfaces

---
+++ src/Interfaces/INarocnik.java
@@ -0,0 +1,7 @@
+package Interfaces;
+
+public interface INarocnik {
+    public void prijavi(IOpazovalec o);
+    public void odjavi (IOpazovalec o);
+    public void obvesti (IOpazovalec o);
+}
---
+++ src/Interfaces/IOpazovalec.java
@@ -0,0 +1,5 @@
+package Interfaces;
+
+public interface IOpazovalec {
+    public void posodobi();
+}
